4027bbcc7113cf3199db5aa2df2cb39a40d31881
Lucas Velloso <lucasabreuvelloso@gmail.com>
2024-05-29 10:40:52 -0300

    fix: path problem

--- src/objects/Dispositivo.py
+++ src/objects/Dispositivo.py
@@ -1,7 +1,15 @@
-from ..Interfaces.Interfaces_dispositivo import InterfaceDispositivo
+import sys
+import os
+
+# Pega o diretorio pai do arquivo
+diretorioPai = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#fornece o caminho
+sys.path.append(diretorioPai)
+
+from Interfaces.Interfaces_dispositivo import  InterfaceDispositivo
 from abc import ABC,abstractmethod
 
-class Dispositivo(ABC,InterfaceDispositivo):
+class Dispositivo(InterfaceDispositivo,ABC):
     def __init__(self,nome:str)->None:
         self.__nome=nome
 
