87acee7edbcb66bee80df08adf9af491215e3fb0
brenopmir <143268744+brenopmir@users.noreply.github.com>
2024-06-05 20:34:02 -0300

    criei a função de inicializar as planilhas para facilitar na main

--- src/objects/planilha.py
+++ src/objects/planilha.py
@@ -1,34 +1,36 @@
 # esse arquivo tem como função criar a planilha inicial 
 from openpyxl import Workbook
 
-wb=Workbook()
-ws=wb.active
+def iniciar_planilhas():
+    wb=Workbook()
+    ws=wb.active
 
-ws.title="Comodo"
+    ws.title="Comodo"
 
-wb.create_sheet("Lampadas")
-wb.create_sheet("Ares Condicionados")
-wb.create_sheet("Janelas")
-wb.create_sheet("Cortinas")
+    wb.create_sheet("Lampadas")
+    wb.create_sheet("Ares Condicionados")
+    wb.create_sheet("Janelas")
+    wb.create_sheet("Cortinas")
 
-ws=wb["Lampadas"]
-ws.append(["Comodo","Nome","Ligado","Temperatura","Intensidade","Abertura","Tranca","Cor"])
+    ws=wb["Lampadas"]
+    ws.append(["Comodo","Nome","Ligado","Temperatura","Intensidade","Abertura","Tranca","Cor"])
 
-ws=wb["Ares Condicionados"]
-ws.append(["Comodo","Nome","Ligado","Temperatura","Intensidade","Abertura","Tranca","Cor"])
+    ws=wb["Ares Condicionados"]
+    ws.append(["Comodo","Nome","Ligado","Temperatura","Intensidade","Abertura","Tranca","Cor"])
 
-ws=wb["Janelas"]
-ws.append(["Comodo","Nome","Ligado","Temperatura","Intensidade","Abertura","Tranca","Cor"])
+    ws=wb["Janelas"]
+    ws.append(["Comodo","Nome","Ligado","Temperatura","Intensidade","Abertura","Tranca","Cor"])
 
-ws=wb["Cortinas"]
-ws.append(["Comodo","Nome","Ligado","Temperatura","Intensidade","Abertura","Tranca","Cor"])
+    ws=wb["Cortinas"]
+    ws.append(["Comodo","Nome","Ligado","Temperatura","Intensidade","Abertura","Tranca","Cor"])
 
-wb.save("Casa.xlsx")
+    wb.save("Casa.xlsx")
 
 
-wb_Casa_comodo=Workbook()
-ws=wb_Casa_comodo.active
+    wb_Casa_comodo=Workbook()
+    ws=wb_Casa_comodo.active
 
-ws.title="Comodos"
-ws.append(["Comodo","Numero Dispositivos"])
-wb_Casa_comodo.save("Casa_comodos.xlsx")
+    ws.title="Comodos"
+    ws.append(["Comodo","Numero Dispositivos"])
+    wb_Casa_comodo.save("Casa_comodos.xlsx")
+
