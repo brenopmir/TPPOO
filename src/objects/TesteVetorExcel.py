d600a34cf437021284bf4ef7125c1312cdffefba
brenopmir <143268744+brenopmir@users.noreply.github.com>
2024-06-12 21:55:25 -0300

    Consegui tirar o vetor da maneira que tu queria Lucas

--- src/objects/TesteVetorExcel.py
+++ src/objects/TesteVetorExcel.py
@@ -39,15 +39,20 @@ for i,row in enumerate(ws_cortinas.iter_rows(min_row=2), start=2):
 
 
 for i,row in enumerate(ws_ares.iter_rows(min_row=2), start=2):
-    nomeArCondicionado.append((f"{str(row[0].value)}",f"{str(row[1].value)}",f"{str(row[2].value)}",f"{str(row[3].value)}",f"{str(row[4].value)}","","",""))
-
+    if(row[2].value=="True"):
+        nomeArCondicionado.append((f"{str(row[0].value)}",f"{str(row[1].value)}","Ligado",f"{str(row[3].value)}",f"{str(row[4].value)}","","",""))
+    else:
+        nomeArCondicionado.append((f"{str(row[0].value)}",f"{str(row[1].value)}","Desligado",f"{str(row[3].value)}",f"{str(row[4].value)}","","",""))
 
 for i,row in enumerate(ws_janelas.iter_rows(min_row=2), start=2):
-    nomeJanelas.append((f"{str(row[0].value)}",f"{str(row[1].value)}","","","",f"{str(row[5].value)}",f"{str(row[6].value)}",""))
+    if(row[6].value=="True"):
+        nomeJanelas.append((f"{str(row[0].value)}",f"{str(row[1].value)}","","","",f"{str(row[5].value)}","Trancado",""))
+    else:
+        nomeJanelas.append((f"{str(row[0].value)}",f"{str(row[1].value)}","","","",f"{str(row[5].value)}","Destrancado",""))
 
 
-print(f"{nomeComodos}\n")
-print(f"{nomeLampadas}\n")
-print(f"{nomeArCondicionado}\n")
-print(f"{nomeJanelas}\n")
-print(f"{nomeCortina}\n")
+#print(f"{nomeComodos}\n")
+#print(f"{nomeLampadas}\n")
+#print(f"{nomeArCondicionado}\n")
+#print(f"{nomeJanelas}\n")
+#print(f"{nomeCortina}\n")
